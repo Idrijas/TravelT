c29a073a297ba74a6cb564f2449af9af68e2dbbd
Monsdrej <ondrejmacus37@gmail.com>
2026-04-07 22:41:36 +0200

    created a "blueprint" for Trip(s), few of the features can be added later, this is work-in-progress version, so we can advance.

---
+++ Travelt/Trip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelt
+{
+    public class Trip
+    {
+
+        public int TripId { get; set; }
+
+        public string NameOfTrip { get; set; }
+
+        public string Country { get; set;  }
+
+        public DateOnly DateFrom { get; set; }
+
+        public DateOnly DateTo { get; set; }
+
+        public int NumberOfPeople { get; set; }
+
+        public string Description { get; set; }
+
+
+        public Trip() { }
+
+
+    }
+}
