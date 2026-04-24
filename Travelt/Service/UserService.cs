bc21116b3cf394b1451a7840d89b44def5d31123
Monsdrej <ondrejmacus37@gmail.com>
2026-04-25 01:58:16 +0200

    Connected C# to database, created functional register function that saves data to database (SignUpPage), redirect to HomePage with username, fixed Date Picker, added age, email and name validation.

--- Travelt/Service/UserService.cs
+++ Travelt/Service/UserService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using MySqlConnector;
 
 namespace Travelt.Service
@@ -22,7 +24,13 @@ namespace Travelt.Service
             }
         }
 
+
+
+
         private readonly DatabaseConnection database_connection = new DatabaseConnection();
+
+
+
 
         public bool Login(string email, string password)
         {
@@ -40,20 +48,47 @@ namespace Travelt.Service
             return result_count > 0;
         }
 
-        public bool Register(string firstName, string lastName, string userName, DateTime dateOfBirth, string email, string password)
-        {
-            // TEMPORARY: Mock sign up logic for testing UI functionality.
-            // TODO: Replace this with a database query (SQL) once backend is connected.
 
-            if (email == "test@test.com" || userName == "admin")
+
+        public bool Register(string firstName, string lastName, string username, string gender, DateTime dateOfBirth, string email, string password)
+        {
+
+            using var connection = database_connection.GetConnection();
+            connection.Open();
+
+            string userExists = "SELECT COUNT(*) FROM user WHERE email = @email OR username = @username";
+            using var check_db = new MySqlCommand(userExists, connection);
+            check_db.Parameters.AddWithValue("@email", email);
+            check_db.Parameters.AddWithValue("@username", username);
+
+            int result_count = Convert.ToInt32(check_db.ExecuteScalar());
+
+            if (result_count > 0)
             {
                 return false;
             }
 
-            return true;
+            string insert_to_db = @"INSERT INTO user (username, email, password_hash, first_name, last_name, date_of_birth, gender)
+                                    VALUES (@username, @email, @password_hash, @first_name, @last_name, @date_of_birth, @gender)";
+
+
+            using var insert_to_db_data = new MySqlCommand(insert_to_db, connection);
+
+            insert_to_db_data.Parameters.AddWithValue("@username", username);
+            insert_to_db_data.Parameters.AddWithValue("@email", email);
+            insert_to_db_data.Parameters.AddWithValue("@password_hash", password);
+            insert_to_db_data.Parameters.AddWithValue("@first_name", firstName);
+            insert_to_db_data.Parameters.AddWithValue("@last_name", lastName);
+            insert_to_db_data.Parameters.AddWithValue("@date_of_birth", dateOfBirth);
+            insert_to_db_data.Parameters.AddWithValue("@gender", gender);
+
+
+            int count_result = insert_to_db_data.ExecuteNonQuery();
+
+            return count_result > 0;
+
+
+
         }
-
-
-
     }
 }
