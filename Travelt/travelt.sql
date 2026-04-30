99341a68662e8698c8b3ce29345b44ee579f4a5e
DriesJ <dries.altt@gmail.com>
2026-04-30 21:26:38 +0200

    just a sql database sync

--- Travelt/travelt.sql
+++ Travelt/travelt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2026 at 01:41 AM
+-- Generation Time: Apr 30, 2026 at 09:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
